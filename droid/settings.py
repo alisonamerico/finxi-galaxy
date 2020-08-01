8a13a03df6ee024a3c846be6a3bba939da4966bf
alisonamerico <alison.americo@gmail.com>
2020-08-01 19:41:14 -0300

    Extracted configuration from variable 'ALLOWED_HOSTS'

--- droid/settings.py
+++ droid/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from decouple import config
+from decouple import Csv, config
 from functools import partial
 import dj_database_url
 
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
