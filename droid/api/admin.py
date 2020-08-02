8920944149b6b7520a3ba48fe24587a4e1e39e8c
alisonamerico <alison.americo@gmail.com>
2020-08-02 13:17:25 -0300

    Configured Admin

---
+++ droid/api/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from droid.api.models import Anunciante, DemandaDePecas
+
+
+@admin.register(Anunciante)
+class AnuncianteAdmin(admin.ModelAdmin):
+    """
+    AnuncianteAdmin
+        list_display - Fields to be viewed in django admin
+        search_fields - Fields that will be viewed for django admin search
+        list_filter - Fields to be filtered
+    """
+
+    list_display = ('name', 'phone_number', 'email', 'created', 'modified')
+    search_fields = ['name', 'phone_number', 'email']
+    ordering = ('-created',)
+
+
+@admin.register(DemandaDePecas)
+class DemandaDePecasAdmin(admin.ModelAdmin):
+    """
+    DemandaDePecasAdmin
+        list_display - Fields to be viewed in django admin
+        search_fields - Fields that will be viewed for django admin search
+        list_filter - Fields to be filtered
+    """
+    list_display = ('descricao_peca', 'endereco_entrega', 'anunciante', 'status_finalizacao')
+    search_fields = ['descricao_peca', 'endereco_entrega', 'anunciante', 'status_finalizacao']
+    ordering = ('-created',)
